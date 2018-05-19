e7003ef620b2f272b34a35b7c8da4bc5c54f2ea4
alexvi88 <37776143+alexvi88@users.noreply.github.com>
2018-05-19 15:07:42 +0300

    19/05/2018

---
+++ settings.py
@@ -0,0 +1,10 @@
+#id of user/group we want to delete posts from. If group, add '-' before id
+owner_id = 'xxxxxxxxx'
+
+#your login and password and vk_stand-alone app_id
+user_login = 'xxx'
+user_password = 'xxx'
+app_id = 'xxx'
+
+vk_api_version = 5.75
+requests_interval = 0.4
